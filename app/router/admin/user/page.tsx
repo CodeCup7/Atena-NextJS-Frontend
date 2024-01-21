87bf643521f018813e5a5c2f8b7e010aa9a831c5
CupCode <97393786+CodeCup7@users.noreply.github.com>
2024-01-21 19:58:50 +0100

    commit

--- app/router/admin/user/page.tsx
+++ app/router/admin/user/page.tsx
@@ -43,12 +43,12 @@ const UsersPage = () => {
                 title='Potwierdź decyzję'
                 content='Czy napewno chcesz usunąć użytkownika ?' />
             <Link
-                className="flex items-center group link link-accent link-hover text-lg space-x-2"
+                className="flex items-center group link link-info hover:link-success text-lg space-x-2"
                 href={{
                     pathname: "/router/admin/user/userview",
                     query: { onEdit: false, userData: JSON.stringify(new User()) }
                 }}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 stroke-link group-hover:stroke-success">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
                 </svg>
                 <p>Dodaj użytkownika</p>
